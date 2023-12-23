fa25771791f68ece0a91f00da269494104f221da
Skyfallin <dimetrius.hightower@gmail.com>
2023-12-22 19:01:23 -0500

    button somehow in the right place

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,17 +1,30 @@
+import { Box, Button } from "@chakra-ui/react"
 import Image from "next/image"
 import React from "react"
 
 const MyComponent = () => {
   return (
-    <div className="-translate-y-1/10 absolute top-1/2 transform">
-      <Image
-        src="/text-logo.png" // Your image path
-        alt="Description"
-        width={500} // Adjust as needed
-        height={300} // Adjust as needed
-        objectFit="cover" // Adjust as needed
-      />
-    </div>
+    <>
+      <Box className="absolute right-0 top-1/2 -translate-y-1/2 transform">
+        <Image
+          src="/text-logo.png"
+          alt="Description"
+          width={700}
+          height={350}
+          objectFit="cover"
+        />
+        <Box className="absolute right-1/2 top-[calc(50%+75px)] translate-x-1/2 transform">
+          <Button
+            className="gradient-button"
+            color="white"
+            w={[50, 100, 150]}
+            borderRadius="full"
+          >
+            PLAY NOW
+          </Button>
+        </Box>
+      </Box>
+    </>
   )
 }
 
