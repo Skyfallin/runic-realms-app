3ea33d3127f38b82a95784ac009638a25766453e
Skyfallin <skyfallin.business@gmail.com>
2023-12-25 20:55:03 -0500

    carousel progress

--- src/components/image-carousel.tsx
+++ src/components/image-carousel.tsx
@@ -1,97 +1,97 @@
-import { Swiper, SwiperSlide } from "swiper/react"
-import { Pagination, Navigation, EffectCoverflow } from "swiper/modules"
-import SwiperCore from "swiper"
-import Image from "next/image"
-import { Box } from "@chakra-ui/react"
+import React, { useState } from "react"
+import { Box, Flex, IconButton, useBreakpointValue } from "@chakra-ui/react"
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa"
 
-// install Swiper modules
-SwiperCore.use([Pagination, Navigation, EffectCoverflow])
-
-export const images = [
-  { src: "/azana-pixel-perfection.png", alt: "First" },
-  { src: "/ignaroths-lair.png", alt: "Second" },
-  { src: "/azana-pixel-perfection.png", alt: "Third" },
-  { src: "/azana-pixel-perfection.png", alt: "Fourth" },
+const images = [
+  "/carousel-images/azana-pixel-perfection.png",
+  "/carousel-images/ignaroths-lair.png",
 ]
 
 const ImageCarousel = () => {
+  const [currentImage, setCurrentImage] = useState(0)
+
+  const handleNext = () => {
+    setCurrentImage((prev) => (prev === images.length - 1 ? 0 : prev + 1))
+  }
+
+  const handlePrev = () => {
+    setCurrentImage((prev) => (prev === 0 ? images.length - 1 : prev - 1))
+  }
+
+  // Adjust the box size based on the breakpoint
+  // const boxSize = useBreakpointValue({ base: "100%", md: "500px" })
+
   return (
-    <Box position="absolute" bottom="0" width={"100%"}>
-      <Swiper
-        navigation
-        centeredSlides
-        slidesPerView={3}
-        loop
-        pagination={{ clickable: false }}
-        effect="coverflow"
-        modules={[Navigation, Pagination]}
-        onSwiper={(swiper) => console.log(swiper)}
-        className="rounded-lg"
+    <Box
+      // bg="purple"
+      // position="relative"
+      // maxW={boxSize}
+      mx="auto"
+      // position="absolute"
+      // bottom={0}
+      overflow="hidden"
+      borderRadius="md"
+      // className="shadow-lg"
+      maxW="55%"
+      mt="8"
+    >
+      <Flex
+        // w="full"
+        // height="30%"
+        transition="transform 0.5s ease-in-out"
+        transform={`translateX(-${currentImage * 100}%)`}
       >
-        {images.map((image, index) => (
-          <SwiperSlide key={index}>
-            <div className="flex h-full w-full items-center justify-center">
-              <Image
-                src={image.src}
-                alt={image.alt}
-                height={225}
-                width={400}
-                className="block  object-cover"
-              />
-            </div>
-          </SwiperSlide>
+        {images.map((src, index) => (
+          <img
+            src={src}
+            alt={`Slide ${index}`}
+            className="h-full w-full object-cover"
+          />
         ))}
-      </Swiper>
+      </Flex>
+
+      {/* Pagination buttons */}
+      {/* <Flex justify="center" bottom={2} w="full">
+        {images.map((_, index) => (
+          <Box
+            key={index}
+            w={2}
+            h={2}
+            bg={currentImage === index ? "whiteAlpha.900" : "whiteAlpha.500"}
+            rounded="full"
+            mx={1}
+            transition="background-color 0.3s"
+            _hover={{ bg: "whiteAlpha.800" }}
+            onClick={() => setCurrentImage(index)}
+          />
+        ))}
+      </Flex> */}
+
+      {/* Icon buttons */}
+      <Flex
+        justify="space-between"
+        align="flex-end"
+        // position="absolute"
+        top="50%"
+        w="full"
+        px={2}
+        transform="translateY(-50%)"
+      >
+        <IconButton
+          aria-label="Previous image"
+          icon={<FaArrowLeft />}
+          onClick={handlePrev}
+          className="hover:bg-opacity-50"
+        />
+        <IconButton
+          aria-label="Next image"
+          icon={<FaArrowRight />}
+          onClick={handleNext}
+          className="hover:bg-opacity-50"
+        />
+      </Flex>
     </Box>
-    // <Swiper
-    //   slidesPerView={3}
-    //   centeredSlides={true}
-    //   navigation={true}
-    //   loop={true}
-    //   pagination={{ clickable: true }}
-    //   effect={"coverflow"}
-    //   // coverflowEffect={{
-    //   //   // rotate: 0,
-    //   //   // stretch: 0,
-    //   //   depth: 100,
-    //   //   modifier: 1,
-    //   //   // slideShadows: false,
-    //   // }}
-    // >
-    //   <SwiperSlide>
-    //     <Image
-    //       src="/azana-pixel-perfection.png"
-    //       alt="Image 4"
-    //       layout="fill"
-    //       objectFit="cover"
-    //     />
-    //   </SwiperSlide>
-    //   <SwiperSlide>
-    //     <Image
-    //       src="/azana-pixel-perfection.png"
-    //       alt="Image 4"
-    //       layout="fill"
-    //       objectFit="cover"
-    //     />{" "}
-    //   </SwiperSlide>
-    //   <SwiperSlide>
-    //     <Image
-    //       src="/azana-pixel-perfection.png"
-    //       alt="Image 4"
-    //       layout="fill"
-    //       objectFit="cover"
-    //     />{" "}
-    //   </SwiperSlide>
-    //   <SwiperSlide>
-    //     <Image
-    //       src="/azana-pixel-perfection.png"
-    //       alt="Image 4"
-    //       layout="fill"
-    //       objectFit="cover"
-    //     />
-    //   </SwiperSlide>
-    // </Swiper>
   )
 }
 
-export { ImageCarousel }
+export default ImageCarousel
--- src/components/logo-content.tsx
+++ src/components/logo-content.tsx
@@ -1,39 +1,42 @@
 import { Box, Button } from "@chakra-ui/react"
 import Image from "next/image"
 import React from "react"
+import ImageCarousel from "./image-carousel"
 
 const LogoContent = () => {
   return (
-    <>
-      <Box
-        display="flex"
-        flexDirection="column"
-        // background="red.500"
-        alignItems="center"
-        justifyContent="center"
-        h="100%"
-        gap="2"
-        className=""
-        // ml="12rem"
-        // mt="16rem"
+    <Box
+      display="flex"
+      flexDirection="column"
+      // background="red.500"
+      alignItems="center"
+      justifyContent="center"
+      h="100%"
+      gap="2"
+      className=""
+      // ml="12rem"
+      // mt="16rem"
+      transform="translateY(-5%)"
+    >
+      <Image
+        src="/text-logo.png"
+        alt="Description"
+        width={700}
+        height={350}
+        objectFit="cover"
+      />
+      <Button
+        className="button box-shadow logo-button"
+        color="white"
+        size="lg"
+        w={[50, 100, 150]}
       >
-        <Image
-          src="/text-logo.png"
-          alt="Description"
-          width={700}
-          height={350}
-          objectFit="cover"
-        />
-        <Button
-          className="button box-shadow logo-button"
-          color="white"
-          size="lg"
-          w={[50, 100, 150]}
-        >
-          PLAY NOW
-        </Button>
-      </Box>
-    </>
+        PLAY NOW
+      </Button>
+      {/* <Box mt="auto"> */}
+      <ImageCarousel />
+      {/* </Box> */}
+    </Box>
   )
 }
 
