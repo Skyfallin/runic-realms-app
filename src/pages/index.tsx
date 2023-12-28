6ec2ab7729c95323f2796ec64eb260799fccbc9d
Skyfallin <dimetrius.hightower@gmail.com>
2023-12-28 00:05:17 -0500

    changing background color, progress on site footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button } from "@chakra-ui/react"
+import { Box, Button, Text } from "@chakra-ui/react"
 import Head from "next/head"
 import React from "react"
 import ImageCarousel from "~/components/image-carousel"
@@ -32,17 +32,9 @@ const Home = () => {
           <Box gridRow={1} gridColumn={2}>
             <ImageCarousel />
           </Box>
-          <Box
-            className="bg-purple-500 opacity-50"
-            gridRow={1}
-            gridColumn={3}
-          ></Box>
-          <Box
-            className="bg-blue-500 opacity-50"
-            gridRow={2}
-            gridColumn={2}
-          ></Box>
-          <Box gridRow={2} gridColumn={3} width={"90%"} mx="auto" my="auto">
+          <Box gridRow={1} gridColumn={3}></Box>
+          <Box gridRow={2} gridColumn={2}></Box>
+          <Box gridRow={2} gridColumn={3} width="90%" mx="auto" my="auto">
             <video
               controls={false}
               autoPlay
@@ -62,7 +54,7 @@ const Home = () => {
             className="box-shadow zoom-on-hover rounded-md"
             gridRow={3}
             gridColumn={2}
-            width={"90%"}
+            width="90%"
             mx="auto"
             my="auto"
           >
@@ -81,14 +73,34 @@ const Home = () => {
             </video>
           </Box>
           <Box className="" gridRow={3} gridColumn={3}></Box>
-          <Box gridRow={4} gridColumn={2} mx="auto" my="auto">
-            <Button size="lg" className="highlight-button box-shadow">
+          <Box
+            display="flex"
+            flexDirection="column"
+            gridRow={4}
+            gridColumn={2}
+            mx="auto"
+            my="auto"
+          >
+            <Text>Support the server with ranks!</Text>
+            <Button
+              size="lg"
+              className="button highlight-button box-shadow"
+              color="white"
+            >
               Take me to the store!
             </Button>
           </Box>
           <Box gridRow={4} gridColumn={3} mx="auto" my="auto">
             <StoreIcons />
           </Box>
+          <Box
+            gridRow={5}
+            className="col-span-4 bg-red-500"
+            mx="auto"
+            my="auto"
+            height="100%"
+            width="100%"
+          ></Box>
         </section>
       </main>
     </>
