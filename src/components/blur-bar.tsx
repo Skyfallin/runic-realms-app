ced4ea585cf0fc2539786c15edf2dc906751f6cd
Skyfallin <dimetrius.hightower@gmail.com>
2024-01-08 15:52:20 -0500

    RR-593 adding logo to blur-bar component

--- src/components/blur-bar.tsx
+++ src/components/blur-bar.tsx
@@ -1,12 +1,30 @@
-import { Box } from "@chakra-ui/react"
+import { Flex } from "@chakra-ui/react"
+import Image from "next/image"
 
 const BlurBar = () => {
   return (
-    <Box
-      className="absolute left-0 right-0 top-0 bg-black/30 backdrop-blur-md"
+    <Flex
+      position="absolute"
+      className="left-0 right-0 top-0 bg-black/30 backdrop-blur-md"
       style={{ zIndex: 2 }}
       height="4.5rem"
-    ></Box>
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Image
+        src="/runic-realms-logo.svg"
+        alt="Runic Realms Logo"
+        width={2800}
+        height={500}
+        style={{
+          maxWidth: "30%",
+          maxHeight: "75%",
+          position: "absolute",
+          left: "4.5rem",
+        }}
+        priority={true}
+      />
+    </Flex>
   )
 }
 
